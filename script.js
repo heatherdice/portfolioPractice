cde41c07df6fbf6d26627d98e785f30d9a0c1b78
Heather Dice <heatherdice22@gmail.com>
2023-03-08 16:44:36 -0500

    Deleted extra pg, added header to projects pg

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function underConstruction() {
-    alert("*Page is currently under construction!* Things are gonna look a little weird for a while... Thank you for your patience!");
-}
+// function underConstruction() {
+//     alert("*Page is currently under construction!* Things are gonna look a little weird for a while... Thank you for your patience!");
+// }
 
 // // make images larger on hover
 // function bigImg(img) {
