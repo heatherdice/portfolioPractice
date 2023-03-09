80d22d117cd298e455de098b542c253e35a2d81e
Heather Dice <heatherdice22@gmail.com>
2023-03-09 08:17:02 -0500

    Moved contact info into footer, styled

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// function underConstruction() {
-//     alert("*Page is currently under construction!* Things are gonna look a little weird for a while... Thank you for your patience!");
-// }
-
-// // make images larger on hover
-// function bigImg(img) {
-//     img.style.width = "12%";
-// }
-// function normalImg(img) {
-//     img.style.width = "10%";
-// }
+function underConstruction() {
+    alert("*Page is currently under construction!* Things are gonna look a little weird for a while... Thank you for your patience!");
+}
