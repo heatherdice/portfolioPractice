ccfb587377d7dadc6e178551ba6032dcf0f5701e
Heather Dice <heatherdice22@gmail.com>
2023-03-20 12:28:33 -0400

    Added comment to JS

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//underConstruction function: for use when live updates are being made
 // function underConstruction() {
 //     alert("*Page is currently under construction!* Things are gonna look a little weird for a while... Thank you for your patience!");
 // }
